f2d5aa91aef96fe4c87a45bdcb41aa85ec5820e4
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2021-07-28 16:45:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  //give physics properties
+  var options={
+    isStatic:true
+  }
+
+  //create a ground body
+  ground = Bodies.rectangle (200,380,400,10,options);
+  //add the ground body into the world
+  World.add(world, ground);
+
+
+
+}
+
+function draw(){
+  background(0);
+  Engine.update(engine);
+
+  rectMode(CENTER);
+  fill("lightgreen")
+  //function for creating reactangular or square bodies
+  rect(ground.position.x, ground.position.y, 400,10);
+
+
+}
+
+
+
+
+
